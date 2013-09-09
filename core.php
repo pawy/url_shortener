9085cdb9beb824a75852f3cb8d7952f1d4d2af9a
pawy <git@pawy.net>
2013-09-09 21:44:17 +0200

    OOP

--- core.php
+++ core.php
@@ -1,37 +1,218 @@
 <?php
 define('SERVER',$_SERVER['SERVER_NAME']);
 
-function get($index, $scope, $default = null)
+//Helper Class
+Class Helper
 {
-    if (is_object($scope))
+    public static function Get($index, $scope, $default = null)
     {
-        return isset($scope->$index) ? $scope->$index : $default;
+        if (is_object($scope))
+        {
+            return isset($scope->$index) ? $scope->$index : $default;
+        }
+        else if (is_array($scope))
+        {
+            return array_key_exists($index, $scope) && !is_null($scope[$index]) && !(empty($scope[$index]) && !is_numeric($scope[$index])) ? $scope[$index] : $default;
+        }
+        return $default;
     }
-    else if (is_array($scope))
+
+    public static function RandString($length, $charset='abcdefghijklmnopqrstuvwxyz')
     {
-        return array_key_exists($index, $scope) && !is_null($scope[$index]) && !(empty($scope[$index]) && !is_numeric($scope[$index])) ? $scope[$index] : $default;
+        $str = '';
+        $count = strlen($charset);
+        while ($length--) {
+            $str .= $charset[mt_rand(0, $count-1)];
+        }
+        return $str;
     }
-    return $default;
+
+    public static function UrlGetContents ($Url) {
+        if (!function_exists('curl_init')){
+            throw new Exception('CURL is not installed!');
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $Url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+
+    public static function Redirect($url)
+    {
+        header('Location: ' . $url);
+    }
 }
 
-function randString($length, $charset='abcdefghijklmnopqrstuvwxyz')
+//Configuration Class
+Class Config
 {
-    $str = '';
-    $count = strlen($charset);
-    while ($length--) {
-        $str .= $charset[mt_rand(0, $count-1)];
-    }
-    return $str;
+    public static $storageDir;
+    public static $deletionEnabled;
+    public static $passwordProtected;
+    public static $passwordMD5Encrypted;
+    public static $loadStatsAsynchronous;
+    public static $sortAlphabetically;
 }
 
-function url_get_contents ($Url) {
-    if (!function_exists('curl_init')){
-        throw new Exception('CURL is not installed!');
+
+Class Shorten
+{
+    /* Static */
+    public static $shorteners = array();
+
+    public static function getAllShorteners()
+    {
+        if(count(self::$shorteners) == 0)
+        {
+            $files = glob(Config::$storageDir . '[a-z]*');
+            //filter out the logfiles, because glob is not able to return files according to REGEX properly
+            $files = array_filter($files, create_function('$item', 'return !strpos($item,".");'));
+
+            //Sort the array of Files, newest first
+            if(!Config::$sortAlphabetically)
+                usort($files, create_function('$a,$b', 'return filemtime($b) - filemtime($a);'));
+
+            foreach($files as $file)
+            {
+                $name = substr($file,strlen(Config::$storageDir));
+                self::$shorteners[] = new Shorten($name);
+            }
+        }
+        return self::$shorteners;
     }
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $Url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    return $output;
+
+    public static function Create($name, $url)
+    {
+        return (new Shorten($name))->save($url);
+    }
+
+    public static function Redirect($name)
+    {
+        try
+        {
+            (new Shorten($name))->redirectToUrl();
+        }
+        catch(Exception $e)
+        {
+            Helper::Redirect('http://' . SERVER);
+        }
+    }
+
+    public static function getRandomShorten()
+    {
+        return Helper::RandString(4);
+    }
+
+    /* Class members */
+    public $name;
+    public $shortenedLink;
+    private $filename;
+    private $logFilename;
+    private $url;
+    private $statistic;
+
+    public function __construct($name)
+    {
+        if(preg_match('/^[a-z ]+$/',$name) != 1)
+            throw new IllegalCharacterException("The shortened url {$name} contains illegal characters. Only a-z is allowed.");
+
+        $this->name = $name;
+        $this->shortenedLink = 'http://' . SERVER . '/' . $name;
+        $this->filename = Config::$storageDir . $name;
+        $this->logFilename = $this->filename . '.log';
+    }
+
+    public function delete()
+    {
+        unlink($this->logFilename);
+        unlink($this->filename);
+    }
+
+    public function getUrl()
+    {
+        if(!$this->url)
+        {
+            $this->url = file_get_contents($this->filename);
+        }
+        return $this->url;
+    }
+
+    public function getStatistics()
+    {
+        if(!$this->statistic)
+        {
+            $this->statistic = new Statistic($this->logFilename);
+        }
+        return $this->statistic;
+    }
+
+    public function getStatisticsJSON()
+    {
+        return (json_encode($this->getStatistics()));
+    }
+
+    protected function redirectToUrl()
+    {
+        if(!file_exists($this->filename))
+            throw new ShortenNotExistsException();
+
+        $this->track();
+        Helper::Redirect($this->getUrl());
+    }
+
+    private function track()
+    {
+        $ip = $_SERVER['REMOTE_ADDR'];
+
+        $statistics =
+            '"' . date('d.m.Y H:i') . '";' .
+            '"' . $ip . '";' .
+            '"' .$_SERVER['HTTP_USER_AGENT'] . '";';
+
+        //Location tracking; See http://ipinfo.io
+        try{
+            $geoLocation = json_decode(Helper::UrlGetContents("http://ipinfo.io/{$ip}/json"));
+            if(is_object($geoLocation))
+            {
+                $statistics .=
+                    '"' . $geoLocation->country . '";' .
+                    '"' . $geoLocation->region . '";' .
+                    '"' . $geoLocation->city . '";' .
+                    '"' . $geoLocation->org . '";' .
+                    '"' . $geoLocation->loc . '";' .
+                    '"' . $geoLocation->hostname . '";';
+            }
+        }catch(Exception $e){}
+
+        file_put_contents($this->logFilename, $statistics . "\n", FILE_APPEND);
+    }
+
+    private function save($url)
+    {
+        if(file_exists($this->filename))
+            throw new ShortenAlreadyExistsException("Shorten url {$this->getUrl()} already exists.");
+
+        file_put_contents($this->filename,$url);
+        file_put_contents($this->logFilename,'');
+        return $this;
+    }
 }
+
+Class Statistic
+{
+    public $numberOfHits;
+    public $entries;
+
+    public function __construct($logFilename)
+    {
+        $this->entries = str_replace("\n",'<br>',file_get_contents($logFilename));
+        $this->numberOfHits = substr_count($this->entries,'<br>');
+    }
+}
+
+//Exceptions
+Class ShortenNotExistsException extends Exception{}
+Class ShortenAlreadyExistsException extends Exception{}
+Class IllegalCharacterException extends Exception{}
--- shorten.php
+++ shorten.php
@@ -2,90 +2,65 @@
 require_once('core.php');
 
 //configure
-define('STORAGE_DIR','s/');
-$enableDeletion = true;
-$enablePasswordProtection = false;
-$passwordMd5Encrypted = 'bdc95b7532e651f3c140b95942851808';
+Config::$storageDir = 's/';
+Config::$deletionEnabled = true;
+Config::$passwordProtected = false;
+Config::$passwordMD5Encrypted = 'bdc95b7532e651f3c140b95942851808';
+Config::$loadStatsAsynchronous = false;
+Config::$sortAlphabetically = false;
 
 //open shortened link
-if($shorten = get('shorten',$_GET))
+if($name = Helper::Get('redirect',$_GET))
 {
-    if(file_exists($urlFileName = STORAGE_DIR . $shorten))
-    {
-        $link = file_get_contents($urlFileName);
-
-        $ip = $_SERVER['REMOTE_ADDR'];
-
-        $statistics =
-            '"' . date('d.m.Y H:i') . '";' .
-            '"' . $ip . '";' .
-            '"' .$_SERVER['HTTP_USER_AGENT'] . '";';
-
-        //Location tracking; See http://ipinfo.io
-        try{
-            $geoLocation = json_decode(url_get_contents("http://ipinfo.io/{$ip}/json"));
-            if(is_object($geoLocation))
-            {
-                $statistics .=
-                    '"' . $geoLocation->country . '";' .
-                    '"' . $geoLocation->region . '";' .
-                    '"' . $geoLocation->city . '";' .
-                    '"' . $geoLocation->org . '";' .
-                    '"' . $geoLocation->loc . '";' .
-                    '"' . $geoLocation->hostname . '";';
-            }
-        }catch(Exception $e){}
-
-        file_put_contents("{$urlFileName}.log",
-            $statistics . "\n",
-            FILE_APPEND);
-
-        die(header('Location: ' . $link));
-    }
-    die(header('Location: http://' . SERVER));
+    Shorten::Redirect($name);
 }
 
-if($enablePasswordProtection)
+//asynchronous request for statistics
+if($shorten = Helper::Get('getLog',$_POST))
+{
+    die((new Shorten($shorten))->getStatisticsJSON());
+}
+
+//Password protection
+if(Config::$passwordProtected)
 {
     session_start();
-    if($pw = get('pw',$_POST))
+    if($pw = Helper::Get('pw',$_POST))
     {
         $_SESSION['pw'] = md5($pw);
     }
 
-    if(get('pw',$_SESSION,'') != $passwordMd5Encrypted)
+    if(Helper::Get('pw',$_SESSION,'') != Config::$passwordMD5Encrypted)
     {
-        die(header('Location: shortenerlogin.html'));
+        Helper::Redirect('shortenerlogin.html');
     }
 }
-
-$randomShorten = randString(4);
 
 //create shortened Link
-if($url = get('url',$_POST))
+if($url = Helper::Get('url',$_POST))
 {
-    $shorten = get('shorten',$_POST);
-
-    if(!file_exists($urlFileName = STORAGE_DIR . $shorten))
+    try
     {
-        file_put_contents($urlFileName,$url);
-        file_put_contents("{$urlFileName}.log",'');
-        die(header("Location: /shorten.php#{$shorten}"));
+        $shorten = Shorten::Create(Helper::Get('shorten',$_POST), $url);
+        Helper::Redirect("/short#{$shorten->name}");
     }
-    else
+    catch(Exception $e)
     {
-        $message = "Shorten url '{$shorten}' already exists";
+        $message = $e->getMessage();
     }
 }
 
-//delete shortened Link (depending on $enableDeletion)
-if($enableDeletion && $toDelete = get('delete',$_GET))
+//delete shortened Link (depending on Config::$deletionEnabled)
+if(Config::$deletionEnabled && $toDelete = Helper::Get('delete',$_GET))
 {
-    if(file_exists(STORAGE_DIR . $toDelete. '.log'))
+    try
     {
-        unlink(STORAGE_DIR . $toDelete);
-        unlink(STORAGE_DIR . $toDelete. '.log');
-        die(header("Location: /short"));
+        (new Shorten($toDelete))->delete();
+        Helper::Redirect('/short');
+    }
+    catch(Exception $e)
+    {
+        $message = $e->getMessage();
     }
 }
 ?>
@@ -98,77 +73,121 @@ if($enableDeletion && $toDelete = get('delete',$_GET))
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 
-    <link rel="stylesheet" href="shorten.css">
+    <style>
+        h2
+        {
+            font-weight: bold;
+        }
+        a:hover
+        {
+            text-decoration: none;
+        }
+        .badge-s
+        {
+            background-color: #428bca;
+        }
+        .badge-s:hover
+        {
+            background-color: #2a6496;
+        }
+        .form-control-short
+        {
+            width: 200px;
+        }
+        .margin-left-10
+        {
+            margin-left: 10px;
+        }
+        .statistics
+        {
+            font-size: 10px;
+        }
+    </style>
 
 </head>
-<body>
-    <div class="container">
-        <h1><?= strtoupper(SERVER) ?> <small>URL shortener</small></h1>
-        <?php
-            if(isset($message)):
-        ?>
-            <div class="alert alert-danger"><?= $message ?></div>
-        <?php
-            endif;
-        ?>
-        <form method="post" role="form" class="well">
-            <div class="form-group">
-                <label for="shorten">Shortened URL <small>http://<?= SERVER ?>/</small></label>
-                <input class="form-control form-control-short" onclick="select()" type="text" name="shorten" id="shorten" value="<?= $randomShorten ?>" placeholder="Shortener URL..." />
-            </div>
-            <div class="form-group">
-                <label for="url">URL</label>
-                <input class="form-control" type="url" name="url" id="url" placeholder="Target URL..." />
-            </div>
-            <input type="submit" class="btn btn-primary" value="Shorten URL" />
-        </form>
-        <input id="search" type="search" class="form-control" placeholder="Search..." onclick="select()" />
-        <?php
-            $files = glob(STORAGE_DIR . '[a-z]*');
-            //filter out the logfiles, because glob is not able to return files according to REGEX properly
-            $files = array_filter($files, create_function('$item', 'return !strpos($item,".");'));
-            //Sort the array of Files, newest first
-            usort($files, create_function('$a,$b', 'return filemtime($b) - filemtime($a);'));
+<body data-spy="scroll" data-target="#navbar-main">
 
-            foreach($files as $file):
-                $shorten = substr($file,strlen(STORAGE_DIR));
-                $url = file_get_contents($file);
-                $shortenedLink = 'http://' . SERVER . '/' . $shorten;
-                $logFile = file_get_contents($file . '.log');
+<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse navbar-main-collapse" id="navbar-main">
+        <ul class="nav navbar-nav">
+            <?php
+            foreach(Shorten::getAllShorteners() as $shorten):
+                ?>
+                <li><a href="#<?= $shorten->name ?>"><?= $shorten->name ?></a></li>
+            <?php
+            endforeach;
+            ?>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    <h1><?= strtoupper(SERVER) ?> <small>URL shortener</small></h1>
+    <?php
+    if(isset($message)):
         ?>
-        <section id="<?= $shorten ?>">
+        <div class="alert alert-danger"><?= $message ?></div>
+    <?php
+    endif;
+    ?>
+    <form method="post" role="form" class="well">
+        <div class="form-group">
+            <label for="shorten">Shortened URL <small>http://<?= SERVER ?>/</small></label>
+            <input class="form-control form-control-short" onclick="select()" type="text" name="shorten" id="shorten" value="<?= Shorten::getRandomShorten() ?>" placeholder="Shortener URL..." required />
+        </div>
+        <div class="form-group">
+            <label for="url">URL</label>
+            <input class="form-control" type="url" name="url" id="url" placeholder="Target URL..." required />
+        </div>
+        <input type="submit" class="btn btn-primary" value="Shorten URL" />
+    </form>
+    <input id="search" type="search" class="form-control" placeholder="Search..." onclick="select()" />
+    <?php
+    foreach(Shorten::getAllShorteners() as $shorten):
+        ?>
+        <section id="<?= $shorten->name ?>">
             <h2>
-                <a href="<?= $shortenedLink ?>" title="Open shortened URL" target="_blank">
-                    <?= $shorten ?>
+                <a href="<?= $shorten->shortenedLink ?>" title="Open shortened URL" target="_blank">
+                    <?= $shorten->name ?>
                 </a>
             </h2>
             <p>
-                <input type="text" class="form-control input-sm shorten" value="<?= $shortenedLink ?>" />
+                <input type="text" class="form-control input-sm shorten" value="<?= $shorten->shortenedLink ?>" />
             </p>
             <p>
-                <a title="Show statistics" class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#stats<?= $shorten ?>">
-                    <span class="badge badge-s"><?= substr_count($logFile,"\n") ?></span>
+                <a title="Show statistics" class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#stats<?= $shorten->name ?>">
+                    <span class="badge badge-s" shorten="<?= $shorten->name ?>">
+                        <?= Config::$loadStatsAsynchronous ? '?' : $shorten->getStatistics()->numberOfHits ?>
+                    </span>
                 </a>
-                <?= $url ?>
-                <a class="margin-left-10" href="<?= $url ?>" target="_blank" title="Open the URL">
+                <?= $shorten->getUrl() ?>
+                <a class="margin-left-10" href="<?= $shorten->getUrl() ?>" target="_blank" title="Open the URL">
                     <span class="glyphicon glyphicon-tag"></span>
                 </a>
-            <?php if($enableDeletion): ?>
-                <a href="shorten.php?delete=<?= $shorten ?>" title="Delete the shortened URL" onclick="return confirm('Are you sure?')">
-                    <span class="glyphicon glyphicon-remove-circle"></span>
-                </a>
-            <?php endif; ?>
+                <?php if(Config::$deletionEnabled): ?>
+                    <a href="shorten.php?delete=<?= $shorten->name ?>" title="Delete the shortened URL" onclick="return confirm('Are you sure?')">
+                        <span class="glyphicon glyphicon-remove-circle"></span>
+                    </a>
+                <?php endif; ?>
             </p>
-            <div id="stats<?= $shorten ?>" class="collapse well well-sm statistics">
+            <div id="stats<?= $shorten->name ?>" class="collapse well well-sm statistics">
                 <p>
-                    <?= str_replace("\n",'<br>',$logFile) ?>
+                    <?= Config::$loadStatsAsynchronous ? '' : $shorten->getStatistics()->entries ?>
                 </p>
             </div>
         </section>
-        <?php
-            endforeach;
-        ?>
-    </div>
+    <?php
+    endforeach;
+    ?>
+</div>
 
 <!-- jQuery -->
 <script src="//code.jquery.com/jquery.min.js"></script>
@@ -178,7 +197,100 @@ if($enableDeletion && $toDelete = get('delete',$_GET))
 <!-- Bootstrap: Latest compiled and minified JavaScript -->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 
-<script src="shorten.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+
+        // Search
+        $('#search').keyup(function() {
+            $searchString = $(this).val();
+            $('section').each(function() {
+                if($(this).attr('id').toLowerCase().indexOf($searchString.toLowerCase()) >= 0)
+                    $(this).show(500);
+                else
+                    $(this).hide(500);
+            });
+            setTimeout(function(){
+                setZclip();
+            }, 500 );
+        });
+
+        <?php
+            if(Config::$loadStatsAsynchronous) :
+        ?>
+        //Load Statistics with asynchronous request
+        $('.badge').click( function(){
+            $shorten = $(this).attr('shorten');
+            //only query when showing, not when hiding
+            if($('#stats'+$shorten).hasClass('collapse'))
+            {
+                $this = $(this);
+                $.ajax({
+                    type: 'POST',
+                    url: 'shorten.php',
+                    data: {getLog: $shorten},
+                    success: function(response)
+                    {
+                        $json_response = $.parseJSON(response);
+                        $this.html($json_response.numberOfHits);
+                        $('#stats'+$shorten).find('p').html($json_response.entries);
+                        $this.animate({backgroundColor:'red'},500).animate({backgroundColor:'#428bca'},500);
+                        setTimeout(function(){
+                            setZclip();
+                        }, 500 );
+                    }
+                })
+            }
+        });
+        <?php
+            else:
+         ?>
+        // Catch show toggle events from Bootstrap to reposition the zclip flash
+        $('.statistics').on('shown.bs.collapse', function () {
+            setTimeout(function(){
+                setZclip();
+            }, 200 );
+        });
+        <?php
+            endif;
+        ?>
+        // Catch hide toggle events from Bootstrap to reposition the zclip flash
+        $('.statistics').on('hidden.bs.collapse', function () {
+            setTimeout(function(){
+                setZclip();
+            }, 200 );
+        });
+
+        // Copy to Clipboard using Jquery ZClip plugin; see http://www.steamdev.com/zclip/
+        $('input.shorten').zclip({
+            setHandCursor: false,
+            path:'http://davidwalsh.name/demo/ZeroClipboard.swf',
+            copy:function(){return $(this).val();},
+            afterCopy:function(){
+                $this = $(this);
+                $before = $(this).val();
+                $(this).val('Copied to Clipboard');
+                setTimeout(function() {
+                    $this.val($before);
+                },1000);
+                $(this).animate({backgroundColor:'red'},500).animate({backgroundColor:'white'},500);
+            }
+        });
+    });
+
+    // Reposition the zClip's Flash overlay to catch the click
+    function setZclip()
+    {
+        $('input.shorten').zclip('remove');
+        $('input.shorten').zclip({
+            setHandCursor: false,
+            path:'http://davidwalsh.name/demo/ZeroClipboard.swf',
+            copy:function(){return $(this).val();},
+            //the triggered function fires allthoug we removed the zClip, therefore we do not need to set it again
+            afterCopy:function(){}
+        });
+    }
+
+</script>
 
 </body>
 </html>
